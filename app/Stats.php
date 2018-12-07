cbc1917be3e802f800ec371d385146facf91a973
jhkiehna <jhkiehna@gmail.com>
2018-12-07 10:17:32 -0500

    created a stats class and pulled method from document there

---
+++ app/Stats.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App;
+
+use App\Document;
+
+class Stats
+{
+    private $document;
+
+    public $numberParagraphs;
+    public $numberSentences;
+    public $numberWords;
+    public $numberCharacters;
+
+    public $averageSentencesPerParagraph;
+    public $averageWordsPerParagraph;
+    public $averageWordsPerSentence;
+    public $averageCharactersPerParagraph;
+
+    public $longestParagraph;
+    public $longestParagraphLength;
+    public $shortestParagraph;
+    public $shortestParagraphLength;
+
+    public $longestSentence;
+    public $longestSentenceLength;
+    public $shortestSentence;
+    public $shortestSentenceLength;
+
+    public function __construct(Document $document)
+    {
+        $this->document = $document;
+
+        $this->initialize();
+    }
+
+    private function initialize()
+    {
+        $this->numberParagraphs     = $this->document->getParagraphs()->count();
+        $this->numberSentences      = $this->document->getSentences()->count();
+        $this->numberWords          = $this->document->getWords()->count();
+        $this->numberCharacters     = $this->countCharacters();
+
+        $this->averageSentencesPerParagraph     = $this->getAverageSentencesPerParagraph();
+        $this->averageWordsPerParagraph         = $this->getAverageWordsPerParagraph();
+        $this->averageWordsPerSentence          = $this->getAverageWordsPerSentence();
+        $this->averageCharactersPerParagraph    = $this->getAverageCharactersPerParagraph();
+
+        $this->longestParagraph = $this->getLongest($this->document->getParagraphs());
+        $this->longestParagraphLength = strlen($this->longestParagraph);
+        $this->shortestParagraph = $this->getShortest($this->document->getParagraphs());
+        $this->shortestParagraphLength = strlen($this->shortestParagraph);
+
+        $this->longestSentence = $this->getLongest($this->document->getSentences());
+        $this->longestSentenceLength = strlen($this->longestSentence);
+        $this->shortestSentence = $this->getShortest($this->document->getSentences());
+        $this->shortestSentenceLength = strlen($this->shortestSentence);
+    }
+
+    private function countCharacters()
+    {
+        return $this->document->getParagraphs()->reduce(function ($carryCharacters, $paragraph) {
+            return $carryCharacters + strlen($paragraph);
+        });
+    }
+
+    private function getAverageSentencesPerParagraph()
+    {
+        return number_format($this->numberSentences / $this->numberParagraphs, 2);
+    }
+
+    private function getAverageWordsPerParagraph()
+    {
+        return number_format($this->numberWords / $this->numberParagraphs, 2);
+    }
+
+    private function getAverageWordsPerSentence()
+    {
+        return number_format($this->numberWords / $this->numberSentences, 2);
+    }
+
+    private function getAverageCharactersPerParagraph()
+    {
+        return number_format($this->numberCharacters / $this->numberParagraphs, 2);
+    }
+
+    public function getLongest($collection)
+    {
+        return $collection->reduce(function ($carry, $current) {
+            if (empty($carry) || (strlen($current) >= strlen($carry))) {
+                return $current;
+            }
+
+            return $carry;
+        });
+    }
+
+    public function getShortest($collection)
+    {
+        return $collection->reduce(function ($carry, $current) {
+            if (empty($carry) || (strlen($current) <= strlen($carry))) {
+                return $current;
+            }
+
+            return $carry;
+        });
+    }
+}
---
+++ tests/Unit/StatsTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Document;
+use App\Stats;
+
+class StatsTest extends TestCase
+{
+    public $statsObj;
+
+    public function SetUp()
+    {
+        $path = realpath(__DIR__. '/../test-fixture.txt');
+        $document = new Document($path);
+        $this->statsObj = new Stats($document);
+    }
+
+    public function testStatsCanBeInitialized()
+    {
+        $this->assertNotNull($this->statsObj);
+        $this->assertNotNull($this->statsObj->numberParagraphs);
+        $this->assertNotNull($this->statsObj->shortestParagraph);
+    }
+
+    public function testStatsCanFindADocumentsTotalNumberOfParagraphs()
+    {
+        $this->assertEquals($this->statsObj->numberParagraphs, 20);
+    }
+
+    public function testStatsCanFindADocumentsTotalNumberOfSentences()
+    {
+        $this->assertEquals($this->statsObj->numberSentences, 235);
+    }
+
+    public function testStatsCanADocumentsTotalNumberOfWords()
+    {
+        $this->assertEquals($this->statsObj->numberWords, 1877);
+    }
+
+    public function testStatsCanFindADocumentsTotalNumberOfCharacters()
+    {
+        $this->assertEquals($this->statsObj->numberCharacters, 12606);
+    }
+
+    public function testStatsCanFindADocumentsAvgNumberOfCharactersPerParagraph()
+    {
+        $this->assertEquals($this->statsObj->averageCharactersPerParagraph, 630.30);
+    }
+
+    public function testStatsCanFindADocumentsAvgNumberOfSentencesPerParagraph()
+    {
+        $this->assertEquals($this->statsObj->averageSentencesPerParagraph, 11.75);
+    }
+
+    public function testStatsCanFindADocumentsAvgNumberOfWordsPerParagraph()
+    {
+        $this->assertEquals($this->statsObj->averageWordsPerParagraph, 93.85);
+    }
+
+    public function testStatsCanFindADocumentsAvgNumberOfWordsPerSentence()
+    {
+        $this->assertEquals($this->statsObj->averageWordsPerSentence, 7.99);
+    }
+
+    public function testStatsCanFindADocumentsShortestParagraph()
+    {
+        $shortestParagraph = 'Maecenas et augue urna. Etiam eu tincidunt odio. Cras interdum sapien nec erat elementum, at facilisis neque molestie. Duis at libero quis erat posuere vulputate. Integer lobortis congue vehicula. Nullam auctor, ligula sit amet pretium blandit, enim neque feugiat dolor, eget tempor ante nisi eu sem. Sed ex arcu, accumsan a mattis vitae, consectetur sed eros.';
+
+        $this->assertEquals($this->statsObj->shortestParagraph, $shortestParagraph);
+        $this->assertEquals($this->statsObj->shortestParagraphLength, 360);
+    }
+
+    public function testStatsCanFindADocumentsLongestParagraph()
+    {
+        $longestParagraph = 'Nam justo neque, condimentum sit amet vulputate ut, semper molestie metus. Cras vel metus sollicitudin, ullamcorper orci at, scelerisque urna. Curabitur orci massa, facilisis a ipsum convallis, tincidunt convallis tortor. Quisque ornare congue ex, eu feugiat enim efficitur nec. Duis id est luctus, mattis metus eu, semper mauris. Donec volutpat lacinia turpis, sit amet commodo urna pharetra vel. Nulla auctor risus non neque accumsan, sit amet commodo lectus vulputate. Sed malesuada et ante nec commodo. Nunc volutpat leo feugiat, placerat risus eu, porta dolor. Aliquam venenatis maximus suscipit. Aenean eu metus elementum, tempus felis vel, mollis lorem. Donec bibendum dui mauris, vel congue nisi elementum eget. Proin porta consectetur ligula, ut ultrices risus laoreet non. Morbi sit amet odio ac sapien gravida tincidunt non eu lectus. Nullam at odio at quam gravida vestibulum. Aliquam nisi mi, iaculis ac neque ac, varius egestas turpis.';
+
+        $this->assertEquals($this->statsObj->longestParagraph, $longestParagraph);
+        $this->assertEquals($this->statsObj->longestParagraphLength, 949);
+    }
+
+    public function testStatsCanFindADocumentsShortestSentence()
+    {
+        $shortestSentence = 'Duis ac leo augue';
+
+        $this->assertEquals($this->statsObj->shortestSentence, $shortestSentence);
+        $this->assertEquals($this->statsObj->shortestSentenceLength, 17);
+    }
+
+    public function testStatsCanFindADocumentsLongestSentence()
+    {
+        $longestSentence = 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum sit amet purus non nisl efficitur vestibulum sed ut eros.';
+
+        $this->assertEquals($this->statsObj->longestSentence, $longestSentence);
+        $this->assertEquals($this->statsObj->longestSentenceLength, 156);
+    }
+
+    public function testCanReturnAListOfOverusedWordsAndTimesUsed()
+    {
+        $words = [];
+        $timesUsed = 0;
+        
+        $this->assertEquals($words, $this->document->overusedWords());
+    }
+
+    public function testDocumentCanReturnAListOfOverusedPhrasesAndTimesUsed()
+    {
+        $phrases = [];
+        $timesUsed = 0;
+
+        $this->assertEquals($phrases, $this->document->overusedPhrases());
+    }
+
+    public function testDocumentCanReturnAListOfAlternativesForAnOverusedWord()
+    {
+        $alternatives = [];
+
+        $this->assertEquals($alternatives, $this->document->wordAlternatives('test'));
+    }
+
+    public function testDocumentCanReturnAListOfAlternativesForAnOverusedPhrase()
+    {
+        $alternatives = [];
+
+        $this->assertEquals($alternatives, $this->document->phraseAlternatives('test placeholder phrase'));
+    }
+
+    public function testDocumentCanReturnAListOfSpellingErrors()
+    {
+        $errors = [];
+
+        $this->assertEquals($errors, $this->document->spellingErrors());
+    }
+
+    public function testDocumentCanReturnAListOfGrammarErrors()
+    {
+        $errors = [];
+
+        $this->assertEquals($errors, $this->document->grammarErrors());
+    }
+}
